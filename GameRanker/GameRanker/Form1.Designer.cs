1a5ae39e996d5b4f5cf33b02386c68571a3eb343
Burton <BurtonGamble@users.noreply.github.com>
2019-04-20 21:09:57 -0700

    Add files via upload
    starting commit/upload

---
+++ GameRanker/GameRanker/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace GameRanker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnLoadChoices = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnLoadChoices
+            // 
+            this.btnLoadChoices.Location = new System.Drawing.Point(857, 455);
+            this.btnLoadChoices.Name = "btnLoadChoices";
+            this.btnLoadChoices.Size = new System.Drawing.Size(75, 34);
+            this.btnLoadChoices.TabIndex = 2;
+            this.btnLoadChoices.Tag = "DONOTREMOVE";
+            this.btnLoadChoices.Text = "Load Choices";
+            this.btnLoadChoices.UseVisualStyleBackColor = true;
+            this.btnLoadChoices.Click += new System.EventHandler(this.btnLoadChoices_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button2.BackgroundImage")));
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button2.Location = new System.Drawing.Point(472, 151);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(460, 215);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button1.Location = new System.Drawing.Point(12, 151);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(460, 215);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(944, 501);
+            this.Controls.Add(this.btnLoadChoices);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Game Ranker 0.7a";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnLoadChoices;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ GameRanker/GameRanker/Form1.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Security;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.Linq;
+
+namespace GameRanker
+{
+    public partial class Form1 : Form
+    {
+        private List<Button> btnList = new List<Button>();
+        private List<int> scoreList = new List<int>();
+        private List<Point> pairList = new List<Point>();
+        private List<Button> orderedList = new List<Button>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ResetEverything()
+        {
+            btnList.Clear();
+            scoreList.Clear();
+            pairList.Clear();
+            orderedList.Clear();
+
+            List<Control> itemsToRemove = new List<Control>();
+            foreach (Control ctrl in Controls)
+            {
+                if (ctrl.Tag != null && ctrl.Tag.ToString() == "dispose")
+                {
+                    itemsToRemove.Add(ctrl);
+                }
+            }
+
+            foreach (Control ctrl in itemsToRemove)
+            {
+                Controls.Remove(ctrl);
+                ctrl.Dispose();
+            }
+        }
+
+        private void btnLoadChoices_Click(object sender, EventArgs e)
+        {
+            DialogResult dialog = MessageBox.Show("Are you sure you want to start over?", "Confirmation", MessageBoxButtons.YesNo); ;
+            if (dialog == DialogResult.Yes)
+            {
+                ResetEverything();
+                OpenFileDialog openFileDialog = new OpenFileDialog();
+                openFileDialog.InitialDirectory = "C:\\Users\\Desktop\\";
+                openFileDialog.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.gif";
+                openFileDialog.RestoreDirectory = true;
+                openFileDialog.Multiselect = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    foreach (String file in openFileDialog.FileNames)
+                    {
+                        try
+                        {
+                            Image loadedImage = Image.FromFile(file);
+                            Button newButton = new Button();
+                            newButton.BackgroundImage = Image.FromFile(file);
+                            newButton.BackgroundImageLayout = ImageLayout.Zoom;
+                            newButton.Visible = false;
+                            btnList.Add(newButton);
+                            this.Controls.Add(newButton);
+                            scoreList.Add(0);
+                        }
+                        catch (SecurityException ex)
+                        {
+                            // The user lacks appropriate permissions to read files, discover paths, etc.
+                            MessageBox.Show("Security error. Please contact your administrator for details.\n\n" +
+                                "Error message: " + ex.Message + "\n\n" +
+                                "Details (send to Support):\n\n" + ex.StackTrace
+                            );
+                        }
+                        catch (Exception ex)
+                        {
+                            // Could not load the image - probably related to Windows file system permissions.
+                            MessageBox.Show("Cannot display the image: " + file.Substring(file.LastIndexOf('\\'))
+                                + ". You may not have permission to read the file, or " +
+                                "it may be corrupt.\n\nReported error: " + ex.Message);
+                        }
+                    }
+
+                    btnLoadChoices.Text = "Restart";
+
+                    foreach (Button b in btnList)
+                    {
+                        b.Width = 460;
+                        b.Height = 215;
+                        b.Top = 151;
+                        b.Left = 12;
+                        b.Click += MyClick;
+                    }
+
+                    CreatePairList(btnList.Count);
+                    pairList.Shuffle();
+                    Main();
+                }
+            }
+        }
+
+        private void CreatePairList(int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                for (int j = count - 1; j > i; j--)
+                {
+                    Point p = new Point(i, j);
+                    pairList.Add(p);
+                }
+            }
+        }
+
+        private void Main()
+        {
+            if (pairList.Count > 0)
+            {
+                this.Text = "Game Ranker 0.7a - " + pairList.Count.ToString();
+                LoadTwoChoices(pairList[0]);
+            }
+            else
+            {
+                this.Text = "Game Ranker 0.7a - Final Results";
+                MakeOrderedList();
+                ShowFinalScores();
+            }
+        }
+
+        private void LoadTwoChoices(Point p)
+        {
+            btnList[p.X].Left = 12;
+            btnList[p.X].Visible = true;
+            btnList[p.Y].Left = 472;
+            btnList[p.Y].Visible = true;
+        }
+
+        private void MakeOrderedList()
+        {
+            foreach (Button b in btnList)
+            {
+                b.Tag = scoreList[btnList.IndexOf(b)].ToString();
+
+                int score = 0;
+                if (Int32.TryParse(b.Tag.ToString(), out score))
+                {
+                    if (score < 10)
+                    {
+                        b.Tag = "0" + scoreList[btnList.IndexOf(b)].ToString();
+                    }
+                }
+            }
+            btnList = btnList.OrderByDescending(b => b.Tag).ToList();
+        }
+
+        private void ShowFinalScores()
+        {
+            for (int i = 0; i < btnList.Count; i++)
+            {
+                Label l = new Label();
+                l.Text = btnList[i].Tag.ToString();
+                //l.Text = (i + 1).ToString();
+                l.Font = new Font("Arial", 11, FontStyle.Bold);
+                l.TextAlign = ContentAlignment.MiddleLeft;
+                l.Height = 43;
+                l.Width = 15;
+                l.Tag = "dispose";
+
+                btnList[i].Height = 43;
+                btnList[i].Width = 92;
+
+                if (i < 9)
+                {
+                    l.Left = 5;
+                    l.Top = 15 + (43 * i) + (10 * i);
+
+                    btnList[i].Left = 20;
+                    btnList[i].Top = 15 + (43 * i) + (10 * i);
+                }
+                else if (i < 18)
+                {
+                    l.Left = 112 + 5;
+                    l.Top = 15 + (43 * (i - 9)) + (10 * (i - 9));
+
+                    btnList[i].Left = 112 + 20;
+                    btnList[i].Top = 15 + (43 * (i - 9)) + (10 * (i - 9));
+                }
+                else if (i < 27)
+                {
+                    l.Left = 112 + 112 + 5;
+                    l.Top = 15 + (43 * (i - 18)) + (10 * (i - 18));
+
+                    btnList[i].Left = 112 + 112 + 20;
+                    btnList[i].Top = 15 + (43 * (i - 18)) + (10 * (i - 18));
+                }
+                else
+                {
+                    l.Left = 112 + 112 + 112 + 5;
+                    l.Top = 15 + (43 * (i - 27)) + (10 * (i - 27));
+
+                    btnList[i].Left = 112 + 112 + 112 + 20;
+                    btnList[i].Top = 15 + (43 * (i - 27)) + (10 * (i - 27));
+                }
+
+                this.Controls.Add(l);
+                btnList[i].Enabled = false;
+                btnList[i].Visible = true;
+                btnList[i].Tag = "dispose";
+            }
+        }
+
+        private void MyClick(object sender, EventArgs e)
+        {
+            Button me = (Button)sender;
+            foreach (Button b in btnList)
+            {
+                if (b == me)
+                {
+                    int myIndex = btnList.IndexOf(b);
+                    scoreList[myIndex]++;
+                }
+                b.Visible = false;
+            }
+            pairList.RemoveAt(0);
+            Main();
+        }
+    }
+
+    internal static class MyExtensions
+    {
+        //Stolen code for shuffling Lists
+        public static void Shuffle<T>(this IList<T> list)
+        {
+            RNGCryptoServiceProvider provider = new RNGCryptoServiceProvider();
+            int n = list.Count;
+            while (n > 1)
+            {
+                byte[] box = new byte[1];
+                do provider.GetBytes(box);
+                while (!(box[0] < n * (Byte.MaxValue / n)));
+                int k = (box[0] % n);
+                n--;
+                T value = list[k];
+                list[k] = list[n];
+                list[n] = value;
+            }
+        }
+    }
+}
